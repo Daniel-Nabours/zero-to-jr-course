0b50b15818f19f51a641dc2a4570c13d29b6d448
daniel nabours <danielnabours@gmail.com>
2022-03-04 16:50:45 -0800

    EOD 3/4/22

--- zero-to-jr/Components/PageTransition.tsx
+++ zero-to-jr/Components/PageTransition.tsx
@@ -1,1 +1,49 @@
-import React from "react";
+import { motion } from "framer-motion"
+import React, { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode
+}
+
+function PageTransition({ children }: Props) {
+
+    const slideRight = {
+        name: 'Slide Right',
+        variants: {
+            initial: {
+                opacity: 0,
+                left: '-100%',
+                scale: 0.6
+            },
+            animate: {
+                opacity: 1,
+                left: 0,
+                scale: 1
+            },
+            exit: {
+                opacity: 0,
+                left: '100%',
+                scale: 0.6
+            }
+        },
+        transition: {
+            duration: 0.7
+        }
+    }
+
+    return ( 
+            <motion.main
+                key={"initTransition"}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={slideRight.transition}
+                variants={slideRight.variants}
+            >
+                {children}
+            </motion.main> 
+
+    )
+}
+
+export default PageTransition
--- zero-to-jr/pages/_app.tsx
+++ zero-to-jr/pages/_app.tsx
@@ -2,44 +2,28 @@ import "../styles/styles.scss"
 import type { AppProps } from 'next/app'
 import { motion } from "framer-motion"
 
-const slideRight = {
-  name: 'Slide Right',
-  variants: {
-    initial: {
-      opacity: 0,
-      left: '-100%',
-      scale: 0.6
-    },
-    animate: {
-      opacity: 1,
-      left: 0,
-      scale: 1
-    },
-    exit: {
-      opacity: 0,
-      left: '100%',
-      scale: 0.6
-    }
-  },
-  transition: {
-    duration: 0.7
-  }
-}
+function MyApp({ Component, pageProps, router }: AppProps) {
 
-function MyApp({ Component, pageProps }: AppProps) {
+
   return (
-    <>
-      <motion.div
-        initial={slideRight.variants.initial}
-        animate={slideRight.variants.animate}
-        exit={slideRight.variants.exit}
-        transition={slideRight.transition}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </>
-
+    <motion.div
+      key={router.route} 
+      initial="initial"
+      animate="animate"
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
   )
 }
+
+
 
 export default MyApp
