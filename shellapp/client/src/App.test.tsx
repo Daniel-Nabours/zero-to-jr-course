a6ecbf700584b100e4bee967318591a03ad61ddf
dnabours <dnabours@kr.af.mil>
2022-03-28 09:05:41 -0700

    moved shellapp

--- shellapp/client/src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-export {} //empty
-
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
--- shellapp/client/src/components/rightbar/rightbar.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import RightBar from './rightbar'
-
-describe("The right sidebar", () => {
-  const { getByTestId } = render(<RightBar />)
-  expect(getByTestId("rightBar")).toBeTruthy()
-})
